7b5829e48e9fafceb91fe9aeaaa43e227e906705
Ferrando_Carlos <cferrando2015@gmail.com>
2023-11-02 21:50:22 -0300

    se realizan estilos CSS + se modifica JS y HTML

--- js/index.js
+++ js/index.js
@@ -1,13 +1,11 @@
 const searchElement = document.getElementById('search');
 const resultElement = document.getElementById('result');
-
+//Se escucha con addEventListener 
 searchElement.addEventListener('keyup',
 
     async function searchPhoto(event) {
         //Se obtiene lo que el usuario va a buscar 
-        //const searchValue = document.getElementById('search').value.toLowerCase();
         const searchValue = event.target.value;
-        //console.log(searchValue);
         //conexión a la URL
         const response = await fetch("https://jsonplaceholder.typicode.com/photos");
         //se pasa los datos a JSON
@@ -16,14 +14,17 @@ searchElement.addEventListener('keyup',
         resultElement.innerHTML = "";
         //filtramos para indexar la búsqueda del usuario
         const searchFilter = data.filter(arr => arr.title.toLowerCase().includes(searchValue));
-
+        //Se evalúa que se haya ingresado algo en el input
         if (searchValue.length > 0) {
+            //se recorren los datos filtrados 
             searchFilter.forEach(element => {
+                //se muestran dinámicamente los resultados 
                 resultElement.insertAdjacentHTML("beforeend",`
-                    <picture>
-                        <img src=${element.thumbnailUrl} alt="Foto" />
-                    </picture>,
-                    <h3>${element.title}</h3>`
+                    <picture class="photo">
+                        <img src="${element.thumbnailUrl}" alt="Foto" />
+                        <h3>${element.title}</h3>
+                    </picture>
+                   `
                 );
             });
         }
